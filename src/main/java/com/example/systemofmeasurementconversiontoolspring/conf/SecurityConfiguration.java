8b2c7d4c16d6927e1630432b9b023d3141688c0d
Willination <traviswilliams130691@gmail.com>
2023-11-21 03:44:39 +0200

    changes

--- src/main/java/com/example/systemofmeasurementconversiontoolspring/conf/SecurityConfiguration.java
+++ src/main/java/com/example/systemofmeasurementconversiontoolspring/conf/SecurityConfiguration.java
@@ -5,40 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration  {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
-                .requestMatchers(HttpMethod.PUT,"/api/**").permitAll()
-                .requestMatchers(HttpMethod.POST,"/api/**").permitAll()
-                .requestMatchers(HttpMethod.DELETE,"/api/**").permitAll()
-        );
-        http.csrf().disable();
-        http.cors();
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
         return http.build();
     }
-
-  /*  protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
-                .requestMatchers(HttpMethod.PUT,"/api/**").permitAll()
-                .requestMatchers(HttpMethod.POST,"/api/**").permitAll()
-                .requestMatchers(HttpMethod.DELETE,"/api/**").permitAll()
-                );
-    *//*    http.csrf().disable();
-        http.cors();*//*
-    }*/
 
     @Bean
     public CorsFilter corsFilter() {
