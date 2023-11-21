999238c5d0f65278808ea6791cc05451fb079c90
Willination <traviswilliams130691@gmail.com>
2023-11-21 04:16:01 +0200

    changes

--- src/main/java/com/example/systemofmeasurementconversiontoolspring/conf/SecurityConfiguration.java
+++ src/main/java/com/example/systemofmeasurementconversiontoolspring/conf/SecurityConfiguration.java
@@ -2,10 +2,7 @@ package com.example.systemofmeasurementconversiontoolspring.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
