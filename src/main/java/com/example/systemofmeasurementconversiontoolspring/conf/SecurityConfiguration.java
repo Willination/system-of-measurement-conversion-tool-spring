93b6d4096208f8526e758c14e123aa5c83bef524
Willination <traviswilliams130691@gmail.com>
2023-11-21 05:23:49 +0200

    changes

--- src/main/java/com/example/systemofmeasurementconversiontoolspring/conf/SecurityConfiguration.java
+++ src/main/java/com/example/systemofmeasurementconversiontoolspring/conf/SecurityConfiguration.java
@@ -2,24 +2,25 @@ package com.example.systemofmeasurementconversiontoolspring.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 public class SecurityConfiguration  {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(withDefaults());
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers(antMatcher("/api/**")).permitAll()
+        ).csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
